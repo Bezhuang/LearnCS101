74f58902f7f9993ed6f0c07f92a18ed11bfb208d
Zhuang zhihao <bezhuang@outlook.com>
2021-02-05 17:42:29 +0800

    NewUpdate20210204

---
+++ Java训练营/AlibabaJava01Demo/src/com/alibaba/Test.java
@@ -0,0 +1,15 @@
+package com.alibaba;
+//阿里Java训练营第1期 第1课练习
+public class Test {
+	//代码执行入口点
+	public static void main(String[] args) {
+		//1.打印字符串
+		System.out.println("Hello World");
+		//2.整数变量18岁
+		int age = 18;
+		System.out.println(age);
+		//3.打印字符串：训练营
+		String s1 = "阿里巴巴Java训练营";
+		System.out.println(s1);
+	}
+}
